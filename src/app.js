aba0db61fee3e1791738a84e4ff53829bbae6ab7
silvinavd <silvinavalverde8@gmail.com>
2021-04-16 13:57:11 +0000

    Excuse entrega

--- src/app.js
+++ src/app.js
@@ -5,7 +5,25 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function Excuse() {
+  var my = ["cat", "brother", "friend"];
+  var action = ["ate", "threw", "lost"];
+  var element = ["clothes", "books", "keys"];
+
+  var whoindex = Math.floor(Math.random() * my.length);
+  var didindex = Math.floor(Math.random() * action.length);
+  var whatindex = Math.floor(Math.random() * element.length);
+
+  return (
+    "My " +
+    my[whoindex] +
+    " " +
+    action[didindex] +
+    " the " +
+    " " +
+    element[whatindex]
+  );
+}
+document.querySelector("#btn").addEventListener("click", () => {
+  document.querySelector("#excuse").innerHTML = Excuse();
+});
